86dd28364195bba57bc172eedade3692e3f38200
DannyDang2201 <phanducminhdang2201@gmail.com>
2022-07-24 16:33:02 +0700

    a

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,6 +1,12 @@
 def insertion_sort(a):
     for i in range(1, len(a)):
-        j = i-1
-        while a[j] > a[j+1] and j >= 0:
-            a[j], a[j+1] = a[j+1], a[j]
+        cur_val = a[i]
+        j = i - 1
+        while j >= 0 and cur_val < a[i]:
+            a[j+1] = a[j]
             j -= 1
+
+
+A = [1, 3, 5, 124, 0, -223, 44, 146]
+insertion_sort(A)
+print(A)
